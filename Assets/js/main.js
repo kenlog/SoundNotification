46b2ec5ebe532c0abf2134c8e7658fb4e8901ffa
Valentino Pesce <valentino@iltuobrand.it>
2018-12-31 17:24:47 +0100

    add pathname

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,4 +1,5 @@
-var baseUrl = window.location.protocol + "//" + window.location.host + "/";
+var getUrl = window.location;
+var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
 
 function soundalert()
 {
