0ee8fd090a4fa004eb9b7a055867d3222096526e
kenlog <valentino@iltuobrand.it>
2019-01-04 18:43:58 +0100

    remove hide()

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -10,7 +10,7 @@ function soundalert()
        success: function(response)
         {
             if (response != "") {
-                $("#soundalert").hide().html(response);
+                $("#soundalert").html(response);
             }
         }
    });
