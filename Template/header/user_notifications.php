abf456241a395928cdfc931c6c1bdc1cddf25f92
kenlog <valentino@iltuobrand.it>
2019-01-04 18:44:48 +0100

    add css class

--- Template/header/user_notifications.php
+++ Template/header/user_notifications.php
@@ -1,6 +1,6 @@
 <span class="notification">
 <?php if ($this->user->hasNotifications()): ?>
-<div id="soundalert"></div>
+<div id="soundalert" class="notification-icon-badge"></div>
     <?= $this->modal->mediumIcon('bell web-notification-icon', t('Unread notifications'), 'SoundNotificationController', 'show', array('plugin' => 'SoundNotification', 'user_id' => $this->user->getId())) ?>
 <?php else: ?>
     <?= $this->modal->mediumIcon('bell', t('My notifications'), 'SoundNotificationController', 'show', array('plugin' => 'SoundNotification', 'user_id' => $this->user->getId())) ?>
